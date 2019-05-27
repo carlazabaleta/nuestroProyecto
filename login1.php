84b8cbc7be778cd5b191b79c9e0de4e007402271
carlazabaleta <50380035+carlazabaleta@users.noreply.github.com>
2019-05-27 15:57:39 -0300

    Add files via upload
    added login1.php, estilos.css, main.js (todo concerniente a login y registro)

---
+++ login1.php
@@ -0,0 +1,48 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale-1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta charset="utf-8">
+    <title>INICIO</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="estilos.css">
+  </head>
+
+  <body>
+    <div class="container-form">
+      <div class="toggle">
+        <span>Crear Cuenta</span>
+      </div>
+
+    <div class="formulario">
+      <h2>Iniciar Sesion</h2>
+      <form class="" action="#" method="post">
+        <input type="text" placeholder="Usuario" required>
+        <input type="password" placeholder="password" required>
+        <input type="submit" value="Iniciar Sesion">
+      </form>
+      </div>
+
+      <div class="formulario">
+        <h2>Crea Tu Cuenta</h2>
+        <form class="" action="#" method="post">
+          <input type="text" placeholder="Usuario" required>
+          <input type="password" placeholder="password" required>
+          <input type="email" placeholder="Your Email" required>
+          <input type="text" placeholder="Telefono" required>
+          <input type="submit" value="Regístrate">
+        </form>
+        </div>
+
+        <div class="reset-password">
+          <a href="#">Olvide mi contraseña </a>
+        </div>
+
+<script  src="js/jquery.js"></script>
+<script  src="js/bootstrap.min.js"></script>
+<script  src="js/main.js"></script>
+  </body>
+</html>
