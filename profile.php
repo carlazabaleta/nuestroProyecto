cb55aad4da3b2647bee4d633e50c43b61dddc4d1
Axel <c.facundoaxel@gmail.com>
2019-05-30 17:28:27 -0300

    Perfil del usuario

---
+++ profile.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+
+<?php
+ ?>
+
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Profile</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+
+  <body id="sky">
+    <div class="row">
+      <header class="col-12">
+        <ul class="nav" id="nav">
+
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <button type="button" class="btn btn-outline-light rounded-pill col-sm-12">Inicio</button>
+            </a>
+          </li>
+
+          <li class="nav-item">
+              <form class="form-inline nav-link col-12">
+                <input class="form-control rounded-pill border-0 col-sm-6 col-3" type="search" placeholder="Buscar">
+                <button class="btn btn-outline-light rounded-pill" type="submit">Buscar</button>
+              </form>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <button type="button" class="btn btn-outline-light rounded-pill col-12">Desconectarse</button>
+            </a>
+          </li>
+        </ul>
+      </header>
+
+      <div class="container row">
+        <aside id="background_usercard" class=" col-4">
+
+          <article class="col-12">
+            <p>
+              <a href="#"><img src="imgs/gear.svg" alt="Gear" class="icon-size"></a>
+            </p>
+            <p class="content-to-right">
+              <img src="imgs/user.svg" alt="User" class="col-sm-6 col-12"><br>
+              <button type="button" class="btn btn-outline-light rounded-pill col-sm-6 col-12">Seguir</button>
+            </p>
+          </article>
+
+          <article class="col-12">
+            <h2 class="content-to-right col-12">NOMBRE COMPLETO</h2>
+            <p class="content-to-right col-12">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          </article>
+
+          <article class="col-12">
+            <p class="content-to-right col-12">
+              <a href="#"><img src="imgs/github.svg" alt="Github" class="icon-size"></a>
+              <a href="#"><img src="imgs/mail.svg" alt="Mail" class="icon-size"></a>
+              <a href="#"><img src="imgs/linkedin.svg" alt="Linkedin" class="icon-size"></a>
+            </p>
+          </article>
+
+          <article>
+            <a href="#sky"><button type="button" class="btn btn-outline-light rounded-pill col-1 go-to-top border-0">Ir Arriba</button></a>
+          </article>
+
+        </aside>
+
+        <section id="projects" class="col-8">
+
+          <div class="media">
+            <img src="imgs/example.png" class="col-4" alt="example">
+            <div class="media-body">
+              <h5 class="mt-0">Media heading</h5>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                </p>
+            </div>
+          </div>
+
+          <div class="media">
+            <img src="imgs/example.png" class="col-4" alt="example">
+            <div class="media-body">
+              <h5 class="mt-0">Media heading</h5>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                </p>
+            </div>
+          </div>
+
+          <div class="media">
+            <img src="imgs/example.png" class="col-4" alt="example">
+            <div class="media-body">
+              <h5 class="mt-0">Media heading</h5>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                </p>
+            </div>
+          </div>
+
+        </section>
+
+  <script src="js/jquery-3.4.1.min.js"></script>
+  <script src="js/bootstrap.js"></script>
+  <script src="js/bootstrap.bundle.js"></script>
+  </div>
+  </body>
+</html>
