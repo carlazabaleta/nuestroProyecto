534bdd8a37ae2453e0d57e8dae9c46d228b015e8
carlazabaleta <50380035+carlazabaleta@users.noreply.github.com>
2019-05-30 18:39:59 -0300

    Add files via upload
    coloque un fondo en el index y coloque el login/registro como un popup en el botón "entrar" del index

--- login1.php
+++ login1.php
@@ -6,12 +6,12 @@
   <head>
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale-1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta charset="utf-8">
-    <title>INICIO</title>
+    <title>LOGIN</title>
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="estilos.css">
   </head>
 
-  <body>
+  <body class="background-body-login">
     <div class="container-form">
       <div class="toggle">
         <span>Crear Cuenta</span>
@@ -40,6 +40,9 @@
         <div class="reset-password">
           <a href="#">Olvide mi contraseña </a>
         </div>
+
+        <a class="btn btn-primary" href="index.php" role="button">BACK</a>
+
 
 <script  src="js/jquery.js"></script>
 <script  src="js/bootstrap.min.js"></script>
--- main.js
+++ main.js
@@ -6,3 +6,8 @@ $('.toggle').click(function(){
     opacity:'toggle'
   },  "slow");
 });
+
+function mostrarMensaje() {
+  var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+}
