be04490e4768981679dcee4c81159e5b17b84899
julimono <50380227+julimono@users.noreply.github.com>
2019-05-31 14:40:55 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,80 +1,38 @@
-<?php  ?>
-
 <!DOCTYPE html>
-<html lang="en" dir="ltr">
+<?php
+session_start();
+$error = '';
+if ($_POST) {
+  require('conexion.php');
+  $query = $conex->prepare('SELECT * FROM usuarios WHERE email = :email');
+  $query->bindValue(':email', $_POST['name']);
+  $query->execute();
+  $usuario = $query->fetch(PDO::FETCH_ASSOC);
+  if($usuario){
+    $_SESSION['id']=$usuario['id'];
+    $_SESSION['email']=$usuario['email'];
+    $_SESSION['nombre']=$usuario['nombre'];
+    header('location:home.php');
+  }else{
+    $error = 'el usuario no existe';
+  }
+}
+
+ ?>
+<html lang="es" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale-1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <title>INDEX</title>
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="estilos.css">
+    <title>Blog</title>
   </head>
-
-  <body class="background-index">
-
-<h1 class="nombreproyecto"></h1>
-
-<center>
-<br><br><br>
-<button class="popup" onclick="mostrarMensaje()">ENTRÁ!
-  <span class="popuptext" id="myPopup">
-      <div class="container-form">
-      <div class="toggle">
-        <span>Crear Cuenta</span>
-      </div>
-
-    <div class="formulario">
-      <h2>Iniciar Sesion</h2>
-      <form class="" action="#" method="post">
-        <input type="text" placeholder="Usuario" required>
-        <input type="password" placeholder="password" required>
-        <input type="submit" value="Iniciar Sesion">
+  <body>
+    <div class="container" style="width:100%;height:100vh;display:flex;justify-content:center;align-items:center;">
+      <form action="" method="POST">
+        <label>Email:</label>
+        <input type="text" name="name" value="">
+        <?php echo $error; ?>
+        <br>
+        <input type="submit" value="Ingresar">
       </form>
-      </div>
-
-      <div class="formulario">
-        <h2>Crea Tu Cuenta</h2>
-        <form class="" action="#" method="post">
-          <input type="text" placeholder="Usuario" required>
-          <input type="password" placeholder="password" required>
-          <input type="email" placeholder="Your Email" required>
-          <input type="text" placeholder="Telefono" required>
-          <input type="submit" value="Regístrate">
-        </form>
-        </div>
-
-        <div class="reset-password">
-          <a href="#">Olvide mi contraseña </a>
-        </div>
-  </span>
-</button>
-</center>
-
-
-<div class="wrapper">
-  <div class="letters">
-    <span class="letter">A</span>
-    <span class="letter">C</span>
-    <span class="letter">E</span>
-    <span class="letter">R</span>
-    <span class="letter">C </span><?= " "?>
-    <span class="letter">A</span>
-    <span class="letter">D</span>
-    <span class="letter">E</span>
-    <span class="letter">N</span>
-    <span class="letter">O</span>
-    <span class="letter">S</span>
-    <span class="letter">O </span>
-    <span class="letter">T</span>
-    <span class="letter">R</span>
-    <span class="letter">O</span>
-    <span class="letter">S</span>
-    <span class="letter">;)</span>
-  </div>
-</div>
-
-    <script  src="js/jquery.js"></script>
-    <script  src="js/bootstrap.min.js"></script>
-    <script  src="js/main.js"></script>
+    </div>
   </body>
 </html>
